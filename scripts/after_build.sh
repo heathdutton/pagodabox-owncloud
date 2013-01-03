06ed81ee48a24623eb30855c3eb731eee8ae8899
Heath Dutton <heathdutton@gmail.com>
2013-01-03 16:19:50 -0500

    Permission correction.

--- scripts/after_build.sh
+++ scripts/after_build.sh
@@ -5,7 +5,7 @@ rsync -avz /var/www/apps/ /var/www/core/apps
 rsync -avz /var/www/3rdparty/ /var/www/core/3rdparty
 
 # Create data folder so that permissions can be applied immediately
-mkdir -m 0777 /var/www/core/data
+# mkdir -m 0777 /var/www/core/data
 
 # Replace setup.php with to use mysql credentials given by Pagoda Box
 cp -f /var/www/core/lib/setup.php /var/www/scripts/setup_original.php
