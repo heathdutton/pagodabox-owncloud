20039b5c4f75efe074543b39e363602e18bf4346
Heath Dutton <heathdutton@gmail.com>
2013-01-04 14:23:41 -0500

    .

--- scripts/after_build.sh
+++ scripts/after_build.sh
@@ -15,7 +15,8 @@ cp -f /var/www/scripts/setup_modified.php /var/www/core/lib/setup.php
 cp -f /var/www/scripts/templates/installation.php /var/www/core/core/templates/installation.php
 
 echo "Modifying .htaccess to prevent issues with Basic Auth."
-echo $'\nOrder Deny, Allow\nAllow from all\nAuthType none\nSatisfy Any\n' >> /var/www/core/.htaccess
+echo $'\nAllow from all\nAuthType none\nSatisfy Any\n' >> /var/www/core/.htaccess
+# Order Deny, Allow\n
 
 echo "Cleaning up Git submodules."
 rm -rf -- /var/www/apps
