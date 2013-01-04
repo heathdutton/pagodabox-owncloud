3c3b09fdb10e3a77001954ea59a7791124e34056
Heath Dutton <heathdutton@gmail.com>
2013-01-04 01:01:47 -0500

    .

--- scripts/after_build.sh
+++ scripts/after_build.sh
@@ -1,16 +1,22 @@
-# Install latest apps - Copy contents of apps submodule into the core apps folder
-#rsync -avz /var/www/apps/ /var/www/core/apps
+#!/bin/bash
 
-# Install necessary 3rd party libraries
-#mkdir -m 0644 /var/www/core/3rdparty
-#rsync -avz /var/www/3rdparty/ /var/www/core/3rdparty
+echo "after_build.sh running."
 
-# Create data folder so that permissions can be applied immediately
+echo "Installing latest official app files."
+rsync -avz /var/www/apps/ /var/www/core/apps
+
+echo "Installing necessary 3rd party library files"
+mkdir -m 0644 /var/www/core/3rdparty
+rsync -avz /var/www/3rdparty/ /var/www/core/3rdparty
+
+# echo "Creating data folder"
 # mkdir -m 0777 /var/www/core/data
 
-# Replace setup.php with to use mysql credentials given by Pagoda Box
-# cp -f /var/www/core/lib/setup.php /var/www/scripts/setup_original.php
-# cp -f /var/www/scripts/setup_modified.php /var/www/core/lib/setup.php
+echo "Altering ownCloud installation files to work with Pagoda Box"
+cp -f /var/www/core/lib/setup.php /var/www/scripts/setup_original.php
+cp -f /var/www/scripts/setup_modified.php /var/www/core/lib/setup.php
+cp -f /var/www/scripts/templates/installation.php /var/www/core/core/templates/installation.php
 
-# Replace installation template to hide credentials/settings request
-# cp -f /var/www/scripts/templates/installation.php /var/www/core/core/templates/installation.php
+echo "Cleaning up"
+rm -rf -- /var/www/apps
+rm -rf -- /var/www/3rdparty
