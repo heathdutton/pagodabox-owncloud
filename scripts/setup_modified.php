25d3c606350035938194ae9675dd63a8795813ae
Heath Dutton <heathdutton@gmail.com>
2013-01-04 03:11:53 -0500

    .

--- scripts/setup_modified.php
+++ scripts/setup_modified.php
@@ -15,7 +15,7 @@ if(isset($_POST['install']) && $_POST['install']=='true') {
     // Merge post with our overriding defaults
     $_POST = array_merge($_POST, $overriding_defaults);
 
-//    die(phpinfo());
+    die(phpinfo());
 }
 
 // Then include the normal setup file (this file was coppied into this location, but has not been modified)
