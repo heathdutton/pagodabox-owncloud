0362709313a131a81a9ce43414966228c7a8557d
Heath Dutton <heathdutton@gmail.com>
2013-01-04 01:40:28 -0500

    .

--- scripts/after_build.sh
+++ scripts/after_build.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-echo "after_build.sh running."
+echo "After-build hook is running."
 
-echo "Installing latest official app files."
+echo "Installing latest official app files. This may take a minute."
 rsync -aHz /var/www/apps/ /var/www/core/apps
 
-echo "Installing necessary 3rd party library files"
+echo "Installing necessary 3rd party library files."
 mkdir -m 0755 /var/www/core/3rdparty
 rsync -aHz /var/www/3rdparty/ /var/www/core/3rdparty
 
-echo "Altering ownCloud installation files to work with Pagoda Box"
+echo "Altering ownCloud installation file to self-configure for Pagoda Box."
 cp -f /var/www/core/lib/setup.php /var/www/scripts/setup_original.php
 cp -f /var/www/scripts/setup_modified.php /var/www/core/lib/setup.php
 cp -f /var/www/scripts/templates/installation.php /var/www/core/core/templates/installation.php
 
-echo "Cleaning up"
+echo "Setting folder permissions."
+chown -R www-data:www-data /var/www/core/config /var/www/core/data
+chown www-data:www-data /var/www/core/apps
+chmod 750 /var/www/core/apps /var/www/core/config
+chmod -R 770 /var/www/core/data
+
+echo "Cleaning up Git submodules."
 rm -rf -- /var/www/apps
 rm -rf -- /var/www/3rdparty
